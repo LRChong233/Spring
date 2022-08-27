35c89a085c9d867c8244cb8e3aea0be532549f8c
BadbadLoli <617361995@qq.com>
2022-08-27 18:51:29 +0800

    添加glfw

--- premake5.lua
+++ premake5.lua
@@ -10,6 +10,12 @@ workspace "Spring"
 
 outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
 
+-- Include directories relative to root folder (solution directory)
+IncludeDir = {}
+IncludeDir["GLFW"] = "Spring/vendor/GLFW/inlcude"
+
+include "Spring/vendor/GLFW"
+
 project "Spring"
 	location "Spring"
 	kind "SharedLib"
@@ -30,7 +36,14 @@ project "Spring"
 	includedirs
 	{
 		"%{prj.name}/src",
-		"%{prj.name}/vendor/spdlog/include"
+		"%{prj.name}/vendor/spdlog/include",
+		"%{IncludeDir.GLFW}"
+	}
+
+	links
+	{
+		"GLFW",
+		"opengl32.lib"
 	}
 
 	filter "system:windows"
