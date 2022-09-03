1040755a80686799936956d235d081f4d5efa818
BadbadLoli <617361995@qq.com>
2022-09-03 15:26:19 +0800

    13.添加Layer和LayerStack

---
+++ Spring/src/Spring/Layer.cpp
@@ -0,0 +1,16 @@
+#include "sppch.h"
+#include "Layer.h"
+
+namespace Spring {
+
+	Layer::Layer(const std::string& debugName)
+		:m_DebugName(debugName)
+	{
+	}
+
+	Layer::~Layer()
+	{
+
+	}
+
+}
---
+++ Spring/src/Spring/Layer.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include "Spring/Core.h"
+#include "Spring/Events/Event.h"
+
+namespace Spring {
+
+	class SPRING_API Layer 
+	{
+	public:
+		Layer(const std::string& name = "Layer");
+		virtual ~Layer();
+
+		virtual void OnAttach() {}
+		virtual void OnDetach() {}
+		virtual void OnUpdate() {}
+		virtual void OnEvent(Event& event) {}
+
+	protected:
+		std::string m_DebugName;
+	};
+
+}
