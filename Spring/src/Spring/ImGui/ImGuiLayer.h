c9c338390098d8e732587d5c7a6d5fe925c36cbf
BadbadLoli <617361995@qq.com>
2022-09-03 21:59:32 +0800

    16.利用EventDispatcher对imgui的事件进行分发

--- Spring/src/Spring/ImGui/ImGuiLayer.h
+++ Spring/src/Spring/ImGui/ImGuiLayer.h
@@ -2,6 +2,10 @@
 
 #include "Spring/Layer.h"
 
+#include "Spring/Events/ApplicationEvent.h"
+#include "Spring/Events/KeyEvent.h"
+#include "Spring/Events/MouseEvent.h"
+
 namespace Spring {
 
 	class SPRING_API ImGuiLayer : public Layer
@@ -14,6 +18,15 @@ namespace Spring {
 		void OnDetach();
 		void OnUpdate();
 		void OnEvent(Event& event);
+	private:
+		bool OnMouseButtonPressedEvent(MouseButtonPressedEvent& e);
+		bool OnMouseButtonReleasedEvent(MouseButtonReleasedEvent& e);
+		bool OnMouseMovedEvent(MouseMovedEvent& e);
+		bool OnMouseScrolledEvent(MouseScrolledEvent& e);
+		bool OnKeyPressedEvent(KeyPressedEvent& e);
+		bool OnKeyReleasedEvent(KeyReleasedEvent& e);
+		bool OnKeyTypedEvent(KeyTypedEvent& e);
+		bool OnWindowResizeEvent(WindowResizeEvent& e);
 	private:
 		float m_Time = 0.0f;
 	};
