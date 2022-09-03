89095b0489c4dbfaa0720b70e3d045d9435f289d
BadbadLoli <617361995@qq.com>
2022-09-03 20:54:39 +0800

    15.添加imgui基础layer

---
+++ Spring/src/Spring/ImGui/ImGuiLayer.cpp
@@ -0,0 +1,32 @@
+#include "sppch.h"
+#include "ImGuiLayer.h"
+
+namespace Spring {
+
+
+
+	ImGuiLayer::ImGuiLayer()
+	{
+	}
+
+	ImGuiLayer::~ImGuiLayer()
+	{
+	}
+
+	void ImGuiLayer::OnAttach()
+	{
+	}
+
+	void ImGuiLayer::OnDetach()
+	{
+	}
+
+	void ImGuiLayer::OnUpdate()
+	{
+	}
+
+	void ImGuiLayer::OnEvent(Event & event)
+	{
+	}
+
+}
---
+++ Spring/src/Spring/ImGui/ImGuiLayer.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include "Spring/Layer.h"
+
+namespace Spring {
+
+	class SPRING_API ImGuiLayer : public Layer
+	{
+	public:
+		ImGuiLayer();
+		~ImGuiLayer();
+
+		void OnAttach();
+		void OnDetach();
+		void OnUpdate();
+		void OnEvent(Event& event);
+	private:
+		float m_Time = 0.0f;
+	};
+
+}
