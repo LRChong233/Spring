f76770a5b35b297e17e7bc29b6004b879cd12641
BadbadLoli <617361995@qq.com>
2022-09-04 16:16:34 +0800

    19.input polling测试代码

--- Spring/src/Spring/Application.cpp
+++ Spring/src/Spring/Application.cpp
@@ -5,6 +5,8 @@
 
 #include <glad/glad.h>
 
+#include "Spring/Input.h"
+
 namespace Spring {
 
 #define BIND_EVENT_FN(x) std::bind(&Application::x, this, std::placeholders::_1)
@@ -59,6 +61,9 @@ namespace Spring {
 
 			for (Layer* layer : m_LayerStack)
 				layer->OnUpdate();
+
+			auto[x, y] = Input::GetMousePosition();
+			SP_CORE_TRACE("{0}, {1}", x, y);
 
 			m_Window->OnUpdate();
 		}
