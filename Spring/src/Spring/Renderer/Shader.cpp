87b95d4257f2daa276380bf14cf84114ebab16f1
LYJD <617361995@qq.com>
2022-10-06 17:43:28 +0800

    44.把shader以文件的方式剥离出来

--- Spring/src/Spring/Renderer/Shader.cpp
+++ Spring/src/Spring/Renderer/Shader.cpp
@@ -5,6 +5,17 @@
 #include "Platform/OpenGL/OpenGLShader.h"
 
 namespace Spring {
+	Shader * Shader::Create(const std::string & filepath)
+	{
+		switch (Renderer::GetAPI())
+		{
+		case RendererAPI::API::None: SP_CORE_ASSERT(false, "RendererAPI::None is currently not supported!"); return nullptr;
+		case RendererAPI::API::OpenGL: return new OpenGLShader(filepath);
+		}
+
+		SP_CORE_ASSERT(false, "Unknown RendererAPI!");
+		return nullptr;
+	}
 
 	Shader* Shader::Create(const std::string& vertexSrc, const std::string& fragmentSrc)
 	{
