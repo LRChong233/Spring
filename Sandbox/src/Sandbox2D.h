27dc318341cd180b90ba68e90cb60458e69b7897
BadbadLoli <617361995@qq.com>
2022-10-16 15:56:11 +0800

    53.添加2d renderer texture

--- Sandbox/src/Sandbox2D.h
+++ Sandbox/src/Sandbox2D.h
@@ -21,7 +21,7 @@ private:
 	Spring::Ref<Spring::VertexArray> m_SquareVA;
 	Spring::Ref<Spring::Shader> m_FlatColorShader;
 
+	Spring::Ref<Spring::Texture2D> m_CheckerboardTexture;
+
 	glm::vec4 m_SquareColor = { 0.2f, 0.3f, 0.8f, 1.0f };
-
-
 };
