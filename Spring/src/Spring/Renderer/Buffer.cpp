b3ad7ad447e90d6848c949774ae9ff1bbe5f9897
BadbadLoli <617361995@qq.com>
2022-10-29 15:16:06 +0800

    61.修改Buffer/RenderAPI等内容为batch rendering做准备

--- Spring/src/Spring/Renderer/Buffer.cpp
+++ Spring/src/Spring/Renderer/Buffer.cpp
@@ -7,12 +7,24 @@
 
 namespace Spring {
 
+	Ref<VertexBuffer> VertexBuffer::Create(uint32_t size)
+	{
+		switch (Renderer::GetAPI())
+		{
+		case RendererAPI::API::None: SP_CORE_ASSERT(false, "RendererAPI::None is currently not supproted!"); return nullptr;
+		case RendererAPI::API::OpenGL: return CreateRef<OpenGLVertexBuffer>(size);
+		}
+
+		SP_CORE_ASSERT(false, "Unknown RendererAPI!");
+		return nullptr;
+	}
+
 	Ref<VertexBuffer> VertexBuffer::Create(float* vertices, uint32_t size)
 	{
 		switch (Renderer::GetAPI())
 		{
 		case RendererAPI::API::None:	SP_CORE_ASSERT(false, "RendererAPI::None is currently not supported!"); return nullptr;
-		case RendererAPI::API::OpenGL:	return std::make_shared<OpenGLVertexBuffer>(vertices, size);
+		case RendererAPI::API::OpenGL:	return CreateRef<OpenGLVertexBuffer>(vertices, size);
 		}
 
 		SP_CORE_ASSERT(false, "Unknown RendererAPI!");
@@ -24,7 +36,7 @@ namespace Spring {
 		switch (Renderer::GetAPI())
 		{
 		case RendererAPI::API::None:	SP_CORE_ASSERT(false, "RendererAPI::None is currently not supported!"); return nullptr;
-		case RendererAPI::API::OpenGL:	return std::make_shared<OpenGLIndexBuffer>(indices, size);
+		case RendererAPI::API::OpenGL:	return CreateRef<OpenGLIndexBuffer>(indices, size);
 		}
 
 		SP_CORE_ASSERT(false, "Unknown RendererAPI!");
