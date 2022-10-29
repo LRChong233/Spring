5c61e8b92b19d8b05879a071f3fd7db6f884466a
BadbadLoli <617361995@qq.com>
2022-10-29 16:10:23 +0800

    61.合批操作，一个drawcall画两个矩形

--- Sandbox/src/Sandbox2D.cpp
+++ Sandbox/src/Sandbox2D.cpp
@@ -44,7 +44,7 @@ void Sandbox2D::OnUpdate(Spring::Timestep ts)
 		Spring::Renderer2D::DrawQuad({ -1.0f,0.0f }, { 0.8f, 0.8f }, { 0.8f, 0.2f, 0.3f, 1.0f });
 		Spring::Renderer2D::DrawQuad({ 0.5f, -0.5f }, { 0.5f, 0.75f }, { 0.2f, 0.3f, 0.8f, 1.0f });
 		//Spring::Renderer2D::DrawQuad({ 0.0f, 0.0f, -0.1f }, { 10.0f, 10.0f }, m_CheckerboardTexture);
-		Spring::Renderer::EndScene();
+		Spring::Renderer2D::EndScene();
 	}
 
 }
